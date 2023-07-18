fc907a6fd61136e0dff27b8f73708d463e374e81
kdkdhoho <hkim4410@naver.com>
2023-07-18 21:55:48 +0900

    test: CI에서 테스트도 동작하는지 확인하기 위한 실패하는 테스트 추가

---
+++ src/test/java/test/CiTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class CiTest {
+
+    @Test
+    void test() {
+        assertThat(1).isEqualTo(2);
+    }
+}
